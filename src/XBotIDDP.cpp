fa830ee78eb44c1e8731abe85f75098480471ae6
Arturo Laurenzi <luca.muratore@iit.it>
2018-03-07 19:09:25 +0100

    XBotIDDP is now working properly with different labels wrt XDDP

--- src/XBotIDDP.cpp
+++ src/XBotIDDP.cpp
@@ -32,82 +32,102 @@ bool XBot::XBotIDDP::init()
     for(auto& c : robot) {
         for(int i=0; i< c.second.size(); i++) {
 
-            XBot::SubscriberIDDP<XBot::RobotState> subscriber_rx(std::string("Motor_id_") + std::to_string(c.second[i]).c_str());
+            XBot::SubscriberIDDP<XBot::RobotState> subscriber_rx(std::string("iddp_Motor_id_") + std::to_string(c.second[i]).c_str());
             fd_read[c.second[i]] = subscriber_rx;
 
-            XBot::PublisherIDDP<XBot::RobotState::pdo_tx> publisher_tx(std::string("rt_in_Motor_id_") + std::to_string(c.second[i]).c_str());
+            XBot::PublisherIDDP<XBot::RobotState::pdo_tx> publisher_tx(std::string("iddp_in_Motor_id_") + std::to_string(c.second[i]).c_str());
             fd_write[c.second[i]] = publisher_tx;
-
-            // initialize the pdo_motor
-            if(fd_read.count(c.second[i])) {
-                XBot::RobotState current_robot_state;
-                while( !(fd_read[c.second[i]].read(current_robot_state)) ) {
-                   sleep(1);
-                }
-                pdo_motor[c.second[i]] = std::make_shared<XBot::RobotState>(current_robot_state);
-            }
         }
     }
 
-    //ft
-    for(auto& ft_j : ft) {
-        // initialize all the fd reading for the ft
-        XBot::SubscriberIDDP<XBot::RobotFT::pdo_rx> subscriber_ft(std::string("Ft_id_") + std::to_string(ft_j.second).c_str());
-        fd_ft_read[ft_j.second] = subscriber_ft;
-        
-        // initialize the pdo_ft
-        if(fd_ft_read.count(ft_j.second)) {
-            XBot::RobotFT::pdo_rx current_FT_state;
-            while( !(fd_ft_read[ft_j.second].read(current_FT_state)) ) {
-                sleep(1);
-            }
-            pdo_FT[ft_j.second] = std::make_shared<XBot::RobotFT::pdo_rx>(current_FT_state);
-        }
-    }
-
-    //imu
-    for(auto& imu_j : imu) {
-        // initialize all the fd reading for the imu
-        XBot::SubscriberIDDP<XBot::RobotIMU::pdo_rx> subscriber_imu(std::string("Imu_id_") + std::to_string(imu_j.second).c_str());
-        fd_imu_read[imu_j.second] = subscriber_imu;
-        
-        // initialize the pdo_IMU
-        if(fd_imu_read.count(imu_j.second)) {
-            XBot::RobotIMU::pdo_rx current_IMU_state;
-            while( !(fd_imu_read[imu_j.second].read(current_IMU_state)) ) {
-                sleep(1);
-            }
-            pdo_IMU[imu_j.second] = std::make_shared<XBot::RobotIMU::pdo_rx>(current_IMU_state);
-        }
-
-    }
-
-    //hand
+    // hand
     for(auto& hand_j : hand) {
         // initialize all the fd reading/writing for the hands
-        XBot::SubscriberIDDP<XBot::RobotState> subscriber_rx(std::string("Motor_id_") + std::to_string(hand_j.second).c_str());
+        XBot::SubscriberIDDP<XBot::RobotState> subscriber_rx(std::string("iddp_Motor_id_") + std::to_string(hand_j.second).c_str());
         fd_read[hand_j.second] = subscriber_rx;
 
-        XBot::PublisherIDDP<XBot::RobotState::pdo_tx> publisher_tx(std::string("rt_in_Motor_id_") + std::to_string(hand_j.second).c_str());
+        XBot::PublisherIDDP<XBot::RobotState::pdo_tx> publisher_tx(std::string("iddp_in_Motor_id_") + std::to_string(hand_j.second).c_str());
         fd_write[hand_j.second] = publisher_tx;
+    }
 
-        // initialize the pdo_motor
-        if(fd_read.count(hand_j.second)) {
-            XBot::RobotState current_robot_state;
-            while( !(fd_read[hand_j.second].read(current_robot_state)) ) {
-                sleep(1);
-            }
-            pdo_motor[hand_j.second] = std::make_shared<XBot::RobotState>(current_robot_state);
+    // ft
+    for(auto& ft_j : ft) {
+        // initialize all the fd reading for the ft
+        XBot::SubscriberIDDP<XBot::RobotFT::pdo_rx> subscriber_ft(std::string("iddp_Ft_id_") + std::to_string(ft_j.second).c_str());
+        fd_ft_read[ft_j.second] = subscriber_ft;
+    }
+
+    // imu
+    for(auto& imu_j : imu) {
+        // initialize all the fd reading for the imu
+        XBot::SubscriberIDDP<XBot::RobotIMU::pdo_rx> subscriber_imu(std::string("iddp_Imu_id_") + std::to_string(imu_j.second).c_str());
+        fd_imu_read[imu_j.second] = subscriber_imu;
+    }
+
+    
+    // motors init
+    for(auto& c : robot) {
+        for(int i=0; i< c.second.size(); i++) {
+
+            // initialize the pdo_motor
+//             if(fd_read.count(c.second[i])) {
+                XBot::RobotState current_robot_state;
+//                 if( !(fd_read[c.second[i]].read(current_robot_state)) ) {
+//                    // give a warning
+//                 }
+                pdo_motor[c.second[i]] = std::make_shared<XBot::RobotState>(current_robot_state);
+//             }
         }
     }
     
+    // hand init
+    for(auto& hand_j : hand) {
+
+        // initialize the pdo_motor
+//         if(fd_read.count(hand_j.second)) {
+            XBot::RobotState current_robot_state;
+//             if( !(fd_read[hand_j.second].read(current_robot_state)) ) {
+//                 // give a warning
+//             }
+            pdo_motor[hand_j.second] = std::make_shared<XBot::RobotState>(current_robot_state);
+//         }
+    }
+    
+    // ft init
+    for(auto& ft_j : ft) {
+
+        // initialize the pdo_ft
+//         if(fd_ft_read.count(ft_j.second)) {
+            XBot::RobotFT::pdo_rx current_FT_state;
+//             if( !(fd_ft_read[ft_j.second].read(current_FT_state)) ) {
+//                 // give a warning
+//             }
+            pdo_FT[ft_j.second] = std::make_shared<XBot::RobotFT::pdo_rx>(current_FT_state);
+//         }
+    }
+    
+    // imu init
+    for(auto& imu_j : imu) {
+
+        // initialize the pdo_IMU
+//         if(fd_imu_read.count(imu_j.second)) {
+            XBot::RobotIMU::pdo_rx current_IMU_state;
+//             if( !(fd_imu_read[imu_j.second].read(current_IMU_state)) ) {
+//                 // give a warning
+//             }
+            pdo_IMU[imu_j.second] = std::make_shared<XBot::RobotIMU::pdo_rx>(current_IMU_state);
+//         }
+
+    }
+    
+
     return true;
 }
 
 void XBot::XBotIDDP::updateTX()
 {
     // Motor + hands
-    for( auto& f: fd_read) {
+    for( auto& f: fd_write) {
 
         // write to the IDDP publisher to command the RobotStateTX in the pdo_motor buffer
         XBot::RobotState::pdo_tx actual_pdo_tx = pdo_motor.at(f.first)->RobotStateTX;
